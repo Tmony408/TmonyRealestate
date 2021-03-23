d52e4e3a26faac79fa07099905da7d0caed7f561
unknown <adetulatestimony@gmail.com>
2021-03-23 08:28:27 +0100

    added host

--- realEstate/settings.py
+++ realEstate/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'rhy#29_$%hlqh-_$jtqdpuw8n9)!7u(f!dsg9=algf^x6ty&_$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [' 192.168.0.103', 'localhost','127.0.0.1']
+ALLOWED_HOSTS = ['tboy1234.pythonanywhere.com']
 
 
 # Application definition
